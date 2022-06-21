015f65a3ad728d8a0756843685a9386f0e0cb6b0
sampathNG <jnanisampath@gmail.com>
2022-06-21 10:53:09 +0530

    first commit

---
+++ day1/p.py
@@ -0,0 +1,15 @@
+# QUESTION 1
+print("Hello, World1")
+# QUESTION 2
+a = int(input())
+b = int(input())
+def x(a,b):
+    print(a+b)
+    print(a-b)
+    print(a*b)
+x(a,b)
+#   QUESTION 3
+def x(a,b):
+    print(a//b)
+    print(a/b)
+x(a,b)
